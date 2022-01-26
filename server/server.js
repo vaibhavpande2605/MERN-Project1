cc540b996b660c906f6e9187939d4784058378b9
Vaibhav <vaibhavpande194@gmail.com>
2022-01-26 15:57:08 +0530

    data base connected

---
+++ server/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+require("dotenv").config();
+
+console.log("hello vaibhav");
+
+const URL = process.env.MONGO_URL;
+
+mongoose.connect(
+  URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    else {
+      return console.log("mongodb connected");
+    }
+  }
+);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port} 🔥`);
+});
+
+// Middlewares
+
+const middleware = (req, res, next) => {
+  console.log("Hello my middleware");
+};
+middleware();
+
+app.get("/", (req, res) => {
+  res.send("Hello from server");
+});
+
+app.get("/about", (req, res) => {
+  res.send("Hello about from server");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Hello contact from server");
+});
+app.get("/signin", (req, res) => {
+  res.send("Hello login from server");
+});
+app.get("/signup", (req, res) => {
+  res.send("Hello signup from server");
+});
