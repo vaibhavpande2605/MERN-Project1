ceb3c6cf13ab71a6de3b58d02678e63b77c43ae9
Vaibhav <vaibhavpande194@gmail.com>
2022-01-26 18:07:53 +0530

    information successfully connected to data base

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,22 +1,47 @@
-const { json } = require('express');
-const express= require('express')
+const { json } = require("express");
+const express = require("express");
 const router = express.Router();
 
+require("../server");
+const User = require("../models/userSchema");
+
 router.get("/", (req, res) => {
-  res.send("Hello from auth,js")
+  res.send("Hello from auth,js");
 });
-
-
 
 router.post("/register", (req, res) => {
+  const { name, email, phone, work, password, confirmpassword } = req.body;
 
+  if (!name || !email || !phone || !work || !password || !confirmpassword) {
+    return res.status(422).json({ error: "fill all the places" });
+  }
 
-console.log( req.body);
-  res.json({message:req.body});
+  User.findOne({ email: email }).then((userExist) => {
+    if (userExist) {
+      return res.status(422).json({ error: "email is already exist" });
+    }
 
- 
+    const user = new User({
+      name,
+      email,
+      phone,
+      work,
+      password,
+      confirmpassword,
+    });
 
+    user
+      .save()
+      .then(() => {
+        return res.status(201).json({ message: "successfully registered" });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "failed to registred" });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 });
 
-
-module.exports= router;
+module.exports = router;
